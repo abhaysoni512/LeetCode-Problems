8ed610a514167d695feccf4f2daff930142e5ec8
Abhay Soni <109501835+abhaysoni512@users.noreply.github.com>
2025-10-12 14:58:05 +0530

    Time: 0 ms (100%), Space: 17.2 MB (15.88%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int size = nums.size();
+       for(int i = 0;i<size;i++){
+        nums.emplace_back(nums[i]);
+       } 
+       return nums;
+    }
+};
