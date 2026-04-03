db9454481d230a1268eebe8a00be12fe1a79467a
Abhay Soni <109501835+abhaysoni512@users.noreply.github.com>
2026-04-03 10:20:10 +0530

    Time: 1 ms (85.72%), Space: 14.8 MB (53.37%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> f;
+        for(auto c:s){
+            f[c]++;
+        }
+        for(int i{}; i<s.size(); ++i){
+            if(f[s[i]]== 1) return i;
+        }
+        
+        
+        return -1;
+
+    }
+};
