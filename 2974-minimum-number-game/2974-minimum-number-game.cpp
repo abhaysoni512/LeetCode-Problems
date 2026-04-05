5512a726eefbbf9bb8b3845b0f44da9c9880b51b
Abhay Soni <109501835+abhaysoni512@users.noreply.github.com>
2026-04-05 22:04:04 +0530

    Time: 0 ms (100%), Space: 28.1 MB (94.65%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -3,16 +3,10 @@ public:
     vector<int> numberGame(vector<int>& nums) {
         vector<int> ans;
         if(nums.size() == 2) return{max(nums[0], nums[1]), min(nums[0], nums[1])};
-        sort(nums.begin(), nums.end());
-        int i{0};
-        int j{1};
-        // 2 3 4 5
-        while(j<nums.size()){
-            ans.push_back(nums[j]);
-            ans.push_back(nums[i]);
-            j = j+2;
-            i = i+2;
+        sort(nums.begin(), nums.end()); // 2 3 4 5
+        for(int i{}; i<nums.size(); i=i+2){
+            swap(nums[i],nums[i+1]); // 3 2
         }
-        return ans;
+        return nums;
     }
 };
