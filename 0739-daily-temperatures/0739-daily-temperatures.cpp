1e74c9509507a399b08167e0c6a70d8bb0127e50
Abhay Soni <109501835+abhaysoni512@users.noreply.github.com>
2026-04-10 20:06:05 +0530

    Time: 13 ms (88.78%), Space: 102.8 MB (81.38%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n);
+        stack<int> st;
+        st.push(n-1);
+        // stack : 3
+        //.     i = 3;   i>=0;
+        for(int i = n-2; i>=0; --i){
+            // check on pocket that if we have small stone throw away
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i] ){
+                st.pop();
+            }
+            if(st.empty()) ans[i] = 0;
+            else{
+                ans[i] = st.top()-i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
