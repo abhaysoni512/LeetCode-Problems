7ae83db3185b18fa6d1a6242a9e4cb6683a8d7db
Abhay Soni <109501835+abhaysoni512@users.noreply.github.com>
2026-06-11 11:53:30 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
-#define a nums
-#define x target
-#define mid guess
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low = 0;
-        int high = a.size() - 1;
-        vector<int> ans{-1, -1};
+        int first = -1, last = -1;
 
+        int low = 0, high = nums.size() - 1;
+
+        // Find first occurrence
         while (low <= high) {
-            int guess = (low + high) / 2;
-            if (a[guess] == x) {
-                int fp = guess;
-                int lp = guess;
-                while (fp > 0 && a[fp] == a[fp - 1]) {
-                    fp--;
-                }
-                ans[0] = fp;
-                while (lp < a.size() - 1 && a[lp] == a[lp + 1]) {
-                    lp++;
-                }
-                ans[1] = lp;
-                return ans;
-            } else if (a[guess] < x) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] < target) {
                 low = mid + 1;
-            } else {
+            }
+            else {
                 high = mid - 1;
             }
         }
 
-        return ans;
+        low = 0;
+        high = nums.size() - 1;
+
+        // Find last occurrence
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return {first, last};
     }
 };
